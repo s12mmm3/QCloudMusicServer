8da23b86ed9ba115834360784b95d8cc02e66b55
overwriter <9856mmm@gmail.com>
2025-04-20 09:34:33 +0800

    refeator: 整理项目结构

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
-#include <QCoreApplication>
+﻿#include <QCoreApplication>
+#include <QLoggingCategory>
 
-int main(int argc, char *argv[])
+#include "server.h"
+#include "generateconfig.h"
+
+Server server;
+void start() {
+    // 检测是否存在 anonymous_token 文件,没有则生成
+    QFile file(QDir(tmpPath).absoluteFilePath("anonymous_token"));
+    if (!file.exists()) {
+        file.open(QIODevice::WriteOnly | QIODevice::Text);
+        file.close();
+    }
+    // 启动时更新anonymous_token
+    generateConfig();
+    server.serveNcmApi({
+        { "checkVersion", true }
+        });
+}
+
+int main(int argc, char* argv[])
 {
+    qSetMessagePattern("%{time yyyy-MM-dd hh:mm:ss.zzz} : %{threadid} : %{category} : %{type} : %{line} : %{function} : %{message}");
+    QLoggingCategory::setFilterRules("QCloudMusicApi.debug=false");
     QCoreApplication a(argc, argv);
-
-    // Set up code that uses the Qt event loop here.
-    // Call a.quit() or a.exit() to quit the application.
-    // A not very useful example would be including
-    // #include <QTimer>
-    // near the top of the file and calling
-    // QTimer::singleShot(5000, &a, &QCoreApplication::quit);
-    // which quits the application after 5 seconds.
-
-    // If you do not need a running Qt event loop, remove the call
-    // to a.exec() or use the Non-Qt Plain C++ Application template.
-
+    start();
     return a.exec();
 }
